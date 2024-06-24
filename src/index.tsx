3a2bd73a6989632056f94188b0089a0d73408e47
Eduardo Larios <eduardo-larios@outlook.com>
2024-06-23 20:24:05 -0700

    Updated app and index routes

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import { render } from "solid-js/web"
 import "./index.css"
 import App from "./App"
 import { Route, Router } from "@solidjs/router";
-import { About, Home, NotFound, Profile } from "@/pages";
+import { About, Home, Login, NotFound, Profile } from "@/pages";
+import { AuthGuard } from "@/components";
 
 render(() => (
     <Router root={App}>
-        <Route path={["/", "home"]} component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/user" component={Profile} />
+        <Route path="/login" component={Login} />
+        <Route component={AuthGuard}>
+            <Route path={["/", "/home"]} component={Home} />
+            <Route path="about" component={About} />
+            <Route path="user" component={Profile} />
+        </Route>
         <Route path="*404" component={NotFound} />
     </Router>
 ), document.getElementById("root")!);
--- src/state/User.ts
+++ src/state/User.ts
@@ -1,16 +1,31 @@
+import { supabase } from "@/supabase";
 import { createMutable } from "solid-js/store";
 
 type User = {
     role: "admin" | "user",
     firstname: string,
     lastname: string,
+    email: string,
     id: string
+}
+
+export function getUserData() {
+    supabase.auth.getUser().then(({ data, error }) => {
+        if (data.user) {
+            const { email, id } = data.user;
+            userStore.email = email || "no-email";
+            userStore.id = id;
+        } else {
+            console.error(error);
+        }
+    })
 }
 
 const userStore = createMutable<User>({
     role: "admin",
     firstname: "Eduardo",
     lastname: "Larios Fernandez",
+    email: "eduardo-larios@outlook.com",
     id: "001"
 });
 
